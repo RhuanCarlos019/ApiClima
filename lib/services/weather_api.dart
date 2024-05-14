5332c5e6ba998fc4be58252e98ff0a7a3c2c2a7c
Rhuan Carlos <123770612+RhuanCarlos019@users.noreply.github.com>
2024-05-14 16:59:05 -0300

    Ok

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -1,22 +1,24 @@
-
-
 import 'dart:convert';
+import 'package:http/http.dart' as http; // Importe o pacote http
 
 import '../models/weather.dart';
 
 class WeatherAPI {
-  get http => null;
+  final String apiKey = '681126f28e7d6fa3a7cfe0da0671e599';
+  final String baseUrl = 'https://api.openweathermap.org/data/2.5';
 
   Future<Clima> buscarDadosClima() async {
-    final response = await http.get('seu_endpoint_da_api_do_clima');
+    final String endpoint = '$baseUrl/weather?q=seu_local&appid=$apiKey&units=metric';
+
+    final response = await http.get(Uri.parse(endpoint));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       return Clima(
-        localizacao: data['localizacao'],
-        temperatura: data['temperatura'],
-        condicaoClimatica: data['condicaoClimatica'],
-        umidade: data['umidade'],
-        velocidadeVento: data['velocidadeVento'],
+        localizacao: data['name'],
+        temperatura: data['main']['temp'],
+        condicaoClimatica: data['weather'][0]['main'],
+        umidade: data['main']['humidity'],
+        velocidadeVento: data['wind']['speed'],
       );
     } else {
       throw Exception('Erro ao carregar dados do clima');
